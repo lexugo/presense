7fa618ce678fc7a31e3232f174d1190a69bd95c4
Hugo <dev@lexugo.ca>
2022-07-18 14:06:53 -0400

    Chameleon · Herbie Hancock
    - Create braise git submodule
    - Record notes in firestore

--- pages/notice.js
+++ pages/notice.js
@@ -1,3 +1,5 @@
+import { record } from 'services/notes'
+
 import { useState } from 'react'
 
 import Brand from 'components/brand'
@@ -5,26 +7,40 @@ import Question from 'components/question'
 
 export default function Notice() {
 	const [note, setNote] = useState()
+	const [context, setContext] = useState()
 
-	function submit(note) {
-		console.log(note) // TODO: save note to database
+	async function submit(note) {
+		await record(note) // TODO: handle error
 		clear()
 	}
 
 	function clear() { setNote(undefined) }
 
 	return (
-		<form className='notice'>
+		<div className='notice'>
 			<Brand />
-			<Question
-				name='note'
-				answer={note}
-				onChange={setNote}
-				onSubmit={submit}
-				placeholder='I was lost in a daydream'
-			>
-				Hello, what did you <span className='keyword'>notice</span> today?
-			</Question>
-		</form>
+			<form className='questions'>
+				<Question
+					name='note'
+					required
+					answer={note}
+					onChange={setNote}
+					onSubmit={submit}
+					placeholder='I was lost in a daydream'
+				>
+					Hello, what did you <span className='keyword'>notice</span> today?
+				</Question>
+				<Question
+					name='context'
+					answer={context}
+					onChange={setContext}
+					onSubmit={submit}
+					placeholder="Yet, I don't remember where I was beforehand"
+				>
+					What was happening in your mind, or around you, at the time?
+				</Question>
+			</form>
+
+		</div>
 	)
 }
