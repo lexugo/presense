451ac1a4936cee34e28e89f3f11eafc51515b8dc
lexugo <dev@lexugo.ca>
2022-07-28 11:21:53 -0400

    Cymande · Brothers On The Slide
    - Show an animated ellipsis on loading
    - Ignore pwa and vscode files

--- components/brand.js
+++ components/brand.js
@@ -1,10 +1,15 @@
+import { useClasses, useSuspense } from 'neon'
+
 import Head from 'next/head'
 
 export default function Brand() {
+	const { suspending } = useSuspense()
+
+	const classNames = useClasses(suspending && 'suspended', 'brand')
 	return (
 		<header>
 			<Head><title>Presense</title></Head>
-			<h1 className='brand'>Pre<span className='sense'>sense</span></h1>
+			<h1 className={classNames}>Pre<span className='sense'>sense</span></h1>
 		</header>
 	)
 }
