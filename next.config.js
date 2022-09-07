35af2547d0292e77179cae77ba1a7481c8d3ec2b
Hugo <dev@lexugo.ca>
2022-09-07 11:53:18 -0400

    Bloc Party · Banquet

--- next.config.js
+++ next.config.js
@@ -1,14 +1,8 @@
-const withPWA = require('next-pwa')
+const progressive = require('next-pwa')({
+	dest: 'public',
+	disable: process.env.NODE_ENV === 'development'
+})
 
-module.exports = withPWA({
-  reactStrictMode: false,
-  redirects: async () => [{
-      source: '/',
-      destination: '/notice',
-      permanent: true
-    }
-  ],
-  pwa: {
-    dest: 'public'
-  }
+module.exports = progressive({
+	reactStrictMode: false
 })
